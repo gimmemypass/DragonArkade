ee2ec11aefc0fc7695bc15bd9bad3f7bd2c8322b
DESKTOP-1R41KQ0\gimmemypass <dgolovenko01@gmail.com>
2024-06-26 01:33:11 +0300

    upd: remove async void from base card ability system

--- Assets/Scripts/Systems/CardAbilities/BaseCardAbilitySystem.cs
+++ Assets/Scripts/Systems/CardAbilities/BaseCardAbilitySystem.cs
@@ -1,4 +1,5 @@
-﻿using Commands;
+﻿using System.Threading.Tasks;
+using Commands;
 using Components;
 using Cysharp.Threading.Tasks;
 using HECSFramework.Core;
@@ -11,14 +12,20 @@ namespace Systems
         {
         }
 
-        public override async void Execute(Entity owner = null, Entity target = null, bool enable = true)
+        public override void Execute(Entity owner = null, Entity target = null, bool enable = true)
+        {
+            ExecuteAsync(owner, target, enable).Forget();
+        }
+
+        private async UniTask ExecuteAsync(Entity owner, Entity target, bool enable)
         {
             var price = Owner.GetComponent<EnergyComponent>().Value;
-            var energyComponent = EntityManager.Default.GetSingleComponent<MainCharacterTagComponent>().Owner.GetComponent<EnergyComponent>();
+            var energyComponent = EntityManager.Default.GetSingleComponent<MainCharacterTagComponent>().Owner
+                .GetComponent<EnergyComponent>();
             energyComponent.ChangeValue(-price);
-            
+
             await ExecuteCard(owner, target, enable);
-            
+
             EntityManager.Default.Command(new CardAbilityHandledCommand());
         }
 
